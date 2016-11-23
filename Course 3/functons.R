a97bfd87674e7784e02d09cbc834b78e72361aeb
wuthmone <whlaing10@gmail.com>
2016-11-23 15:32:08 +0800

    first commit

---
+++ Course 3/functons.R
@@ -0,0 +1,24 @@
+ add2 <- function(x,y){
+   x + y
+ }
+ 
+ above10 <- function(x){
+        use <- x > 10
+        x[use]
+ }
+ 
+columnmean <- function(y){
+  nc <- ncol(y)
+  means <- numeric(nc)
+  for (i in 1:nc) {
+    means[i] <- mean(y[,i], na.rm = T)
+  }
+  means
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
